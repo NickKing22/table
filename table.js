b29b7ed1e93ac40713045711554971c2d72c667c
Nicholas King <108214126+NickKing22@users.noreply.github.com>
2022-08-21 10:49:27 -1000

    Add files via upload

---
+++ table.js
@@ -0,0 +1,8 @@
+//This is the funtion that will be called when the start and stop button are clicked
+function startStop()
+    {
+        //This will enable the start button
+        document.getElementById("startButton").disabled = false
+        //This will disable the stop button
+        document.getElementById("stopButton").disabled = true
+    }
